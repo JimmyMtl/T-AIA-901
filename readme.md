1de40c45e6aac80fe4608106858fc5a25a83d3b1
Jimmy <jimmy.martel@epitech.eu>
2023-02-16 17:03:39 +0100

    update project version

---
+++ readme.md
@@ -0,0 +1,18 @@
+# IA Project
+
+### Installation des librairies
+
+```shell
+pip install -r requirements.txt
+```
+
+### Lancement du projet
+
+```shell
+streamlit run travelOrderApp.py
+```
+
+#### Un nouvel onglet devrait s'ouvrir dans votre navigateur. Si ce n'est pas le cas, vous pouvez accéder à streamlit via l'url suivante:
+[Accéder à l'application]("http://localhost:8501/")
+ou
+[http://localhost:8501]("http://localhost:8501/")
---
+++ travelOrderApp.py
@@ -0,0 +1,49 @@
+import json
+import streamlit as st
+from utils import start_recognition, \
+    start_recognition_with_file, \
+    getShortestPath, \
+    extract_dep_and_arr, \
+    get_gare_name, \
+    getCoordinates
+
+st.title('Welcome to Travel Order')
+st.subheader('Please press start then talk to get your route')
+listening = False
+strlistening = "Analyzing your text.." if listening else "Start listening"
+
+
+# Interface Streamlit
+if st.button(strlistening):
+    phrase = start_recognition()
+    with st.spinner('Loading..'):
+        if phrase:
+            dep, arr = extract_dep_and_arr(phrase)
+
+            if dep is None or arr is None:
+                st.error(
+                    "Cannot find a city departure and/or arrival. It seems that there is no city in your sentence")
+            else:
+                st.write('Vous souhaitez partir de', dep, 'et arrivez à', arr)
+
+                getShortestPath(dep, arr, json.loads(
+                    open("gareGraph/graph.json", "r").read()))
+
+st.text('or you can provide some audio file')
+uploaded_file = st.file_uploader("Glissez votre fichier audio (mp3, flac ou wav)", ["mp3", "flac", "wav"])
+
+if uploaded_file is not None:
+    phrase = start_recognition_with_file(uploaded_file)
+    st.write('You said :', phrase)
+    with st.spinner('Loading..'):
+        if phrase:
+            dep, arr = extract_dep_and_arr(phrase)
+
+            if dep is None or arr is None:
+                st.error(
+                    "Cannot find a city departure and/or arrival. It seems that there is no city in your sentence")
+            else:
+                st.write('Vous souhaitez partir de', dep, 'et arriver à', arr)
+
+                getShortestPath(dep, arr, json.loads(
+                    open("gareGraph/graph.json", "r").read()))
